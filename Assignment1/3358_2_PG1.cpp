85cabd53e9a879c73f809601a4d1f3278307ef2b
RAFAEL\Rafael <rafaelreza@ymail.com>
2017-01-25 14:51:27 -0600

    Wrote program intro

--- Assignment1/3358_2_PG1.cpp
+++ Assignment1/3358_2_PG1.cpp
@@ -1,1 +1,39 @@
+// Roster Number: 2
+//
+// Author Name: Rafael Reza & Cullen
+// Due Date: 01/30/2017
+// Programming Assignment 1
+// Spring 2017 - CS  3358 - Section Number: 2
+// Instructor : Husain Gholoom.
+//
+// Description: Accepts a positive integer greater than 0
+// and less than 200 and runs in through 7 different
+// functions
+#include <iomanip>
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    cout << "Welcome to My APP." << endl;
+    cout << "This APP accepts an integer from the keyboard." << endl;
+    cout << "The APP Calculates and Displays the following: " << endl;
+
+    cout << endl;
+
+    cout << "1. Assuming that the integer represents a $$ amount, " << endl
+         << "then the number of quarters, dimes, nickels, and pennies " << endl
+         << "will be calculated and displayed. " << endl;
+
+    cout << endl;
+
+    cout << "2. Assuming that the integer represents person's age in years then, " << endl
+         << "number of months, days, hours, minutes, and seconds for the person will be " << endl
+         << "calculated and displayed." << endl;
+
+    cout << endl;
+
+    cout << "3. The numbers will be separated into its individual digits." << endl
+         << "Then each digit of the integer will be displayed in English." << endl;
+}
