f9f4844ec2e45be755489735d6481ddd6c17fbc4
PotatoPhusion <potatophusion@gmail.com>
2017-02-08 15:01:59 -0600

    Intro complete

--- Assignment2/3358_2_PG2.cpp
+++ Assignment2/3358_2_PG2.cpp
@@ -1,6 +1,4 @@
-// Roster Numbers: 36, 48
-//
-// Author Names: Rafael Reza & Cullen Sturdivant
+// Author Names: Rafael Reza-36 & Cullen Sturdivant-48
 // Due Date: 02/13/2017
 // Programming Assignment 2
 // Spring 2017 - CS  3358 - Section Number: 2
@@ -15,5 +13,21 @@
 using namespace std;
 
 int main() {
+
+    cout << "Welcome to my perfect matrix program. The function of the program "
+         << "is to:\n" << endl;
+
+    cout << "\t1. Allow the user to enter the size of the perfect matrix, such "
+         << "as n, n >= 2." << endl;
+
+    cout << "\t2. Create a 2D vector array of size N x N." << endl;
+
+    cout << "\t3. Populate the 2D vector array with distinct random numbers."
+         << endl;
+
+    cout << "\t4. Display the sum for each row, column, and diagonals.\n\t   "
+         << "Then determine whether the numbers in N x N vector array are "
+         << "perfect matrix numbers." << endl;
+
     return 0;
 }
