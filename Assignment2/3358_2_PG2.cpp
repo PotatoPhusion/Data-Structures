ab450747fd65631514e52ef3bda55882524747d2
RAFAEL\Rafael <rafaelreza@ymail.com>
2017-02-10 21:10:19 -0600

    So I have the function that prints out the columns and rows but I seriously feel like it doesn't seem right. Mind taking a gander at it and see if its good or not?

--- Assignment2/3358_2_PG2.cpp
+++ Assignment2/3358_2_PG2.cpp
@@ -9,16 +9,23 @@
 #include <iomanip>
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
+void matrix(int);
+
+
 int main() {
+
+    int input;
 
     cout << "Welcome to my perfect matrix program. The function of the program "
          << "is to:\n" << endl;
 
     cout << "\t1. Allow the user to enter the size of the perfect matrix, such "
-         << "as n, n >= 2." << endl;
+         << "as N, n >= 2." << endl;
 
     cout << "\t2. Create a 2D vector array of size N x N." << endl;
 
@@ -27,7 +34,43 @@ int main() {
 
     cout << "\t4. Display the sum for each row, column, and diagonals.\n\t   "
          << "Then determine whether the numbers in N x N vector array are "
-         << "perfect matrix numbers." << endl;
+         << "perfect matrix numbers." << endl << endl;
+
+    cout << "Enter the size of the matrix: ";
+    cin >> input;
+
+    cout << endl;
+
+    cout << "The perfect matrix that is created for size " << input << ":\n";
+
+    matrix(input);
 
     return 0;
+}
+
+void matrix(int input)
+{
+    srand(time(0));
+
+    vector<vector<float> >v;
+
+    for (int i = 0; i < input; i++)
+    {
+        vector<float> w;
+        for (int j = 0; j < input; j++)
+        {
+            w.push_back(rand() % 20);
+        }
+        v.push_back(w);
+    }
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        for (int j = 0; j < v[i].size(); j++)
+        {
+            cout << v[i][j] << " " ;
+        }
+        cout << endl;
+    }
+
 }
