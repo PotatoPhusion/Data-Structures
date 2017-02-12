d0f7ca442880597fcf25c2c97d60ce5ca6bc8569
RAFAEL\Rafael <rafaelreza@ymail.com>
2017-02-12 17:09:31 -0600

    Got the total and first diagonal its own function. BUT it's inside the matrix function. Left some comments for you cause Idk if that's the most efficient method.

--- Assignment2/3358_2_PG2.cpp
+++ Assignment2/3358_2_PG2.cpp
@@ -15,7 +15,7 @@
 using namespace std;
 
 void matrix(int);
-float diagonal1(vector<float>&);
+void diagonal1(vector<vector<int> >);
 
 
 int main() {
@@ -25,17 +25,18 @@ int main() {
     cout << "Welcome to my perfect matrix program. The function of the program "
          << "is to:\n" << endl;
 
-    cout << "\t1. Allow the user to enter the size of the perfect matrix, such "
-         << "as N, n >= 2." << endl;
+    cout << "\t1. Allow the user to enter the size of the perfect matrix, such\n\t "
+         << setw(15) << "as N, n >= 2." << endl;
 
     cout << "\t2. Create a 2D vector array of size N x N." << endl;
 
     cout << "\t3. Populate the 2D vector array with distinct random numbers."
          << endl;
 
-    cout << "\t4. Display the sum for each row, column, and diagonals.\n\t   "
+    cout << "\t4. Display the sum for each row, column, and diagonals.\n\t "
          << "Then determine whether the numbers in N x N vector array are "
-         << "perfect matrix numbers." << endl << endl;
+         << "perfect\n\t"
+         << setw(16) << "matrix numbers." << endl << endl;
 
     cout << "Enter the size of the matrix: ";
     cin >> input;
@@ -51,51 +52,54 @@ int main() {
 
 void matrix(int input)
 {
+    int total = 0;
+    int perfNum = 3;
 
     srand(time(0));
 
-    vector<vector<float> >v;
+    vector<vector<int> >v;
 
     for (int i = 0; i < input; i++)
     {
-        vector<float> w;
+        vector<int> w;
         for (int j = 0; j < input; j++)
         {
-            w.push_back(rand() % 20);
+            w.push_back(rand() % 10);
         }
         v.push_back(w);
     }
+
+    cout << endl;
 
     for (int i = 0; i < v.size(); i++)
     {
         for (int j = 0; j < v[i].size(); j++)
         {
             cout << v[i][j] << " " ;
+            total += v[i][j];
         }
         cout << endl;
     }
 
-    // Here is the diagonal but I just threw it in here for now
-    // I was trying to figure out how to make it a separate function
-    // I kept messing with the parameters and stuff. But I'm not planning
-    // on leaving it here.
-    int diagonal = 0;
-    for(int i = 0; i < v.size(); i++)
-    {
-        //cout << v[i][i] << " ";
-        diagonal += v[i][i];
-    }
-    cout << "diagonal 1 is " << diagonal;
+    cout << endl;
+
+    cout << "The perfect number is: " << (total / perfNum);
+
+    cout << endl;
+
+    diagonal1(v); // here it is my friend, the first diagonal.
 }
 
-/*float diagonal1(vector<float> v)
+//got the diagonal into its own function now but,
+//I placed it in the matrix function. Not sure if that's
+//exactly correct or the style required...
+void diagonal1(vector<vector<int> >v)
 {
     int diagonal = 0;
+
     for(int i = 0; i < v.size(); i++)
     {
-        cout << v[i][i] << " ";
         diagonal += v[i][i];
     }
-    cout << "diagonal 1 is " << diagonal;
+    cout << "Sum of numbers in first diagonal is: " << diagonal << endl;
 }
-*/
