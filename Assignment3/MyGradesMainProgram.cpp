9fed2f53182807a4f83426674ff0b0b34f5938c7
PotatoPhusion <potatophusion@gmail.com>
2017-02-20 14:27:03 -0600

    main's broken, but here it is

--- Assignment3/MyGradesMainProgram.cpp
+++ Assignment3/MyGradesMainProgram.cpp
@@ -6,9 +6,29 @@
 //
 // Grades stuff TODO: Change this
 
+#include <iostream>
+
 using namespace std;
 
 int main() {
+
+    int choice;
+
+    do {
+        try {
+            cout << "Enter your choice: ";
+            cin >> choice;
+            if (cin.fail()) {
+                throw "That is not a number!";
+            }
+        }
+        catch (const char* error) {
+            cerr << "\nERROR:" << error << "\n"
+                 << "Please choose a valid option." << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+    } while (true);
 
 
     return 0;
