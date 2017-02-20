d0b38622c82293b6e0b21f5c67ce42a946c1a7c3
PotatoPhusion <potatophusion@gmail.com>
2017-02-20 13:41:01 -0600

    Created main for assignment 3

---
+++ Assignment3/MyGradesMainProgram.cpp
@@ -0,0 +1,15 @@
+// Author Names: Rafael Reza-36 & Cullen Sturdivant-48
+// Due Date: 02/22/2017
+// Programming Assignment 3
+// Spring 2017 - CS  3358 - Section Number: 2
+// Instructor : Husain Gholoom
+//
+// Grades stuff TODO: Change this
+
+using namespace std;
+
+int main() {
+
+
+    return 0;
+}
