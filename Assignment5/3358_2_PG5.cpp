ba3992a1f6820b88ed36833b7905492c6a38537b
PotatoPhusion <potatophusion@gmail.com>
2017-03-27 15:06:28 -0500

    Intro to Program 5 is complete.

---
+++ Assignment5/3358_2_PG5.cpp
@@ -0,0 +1,28 @@
+// Author Names: Rafael Reza-36 & Cullen Sturdivant-48
+// Due Date: 03/29/2017
+// Programming Assignment 5
+// Spring 2017 - CS  3358 - Section Number: 2
+// Instructor : Husain Gholoom
+//
+// WE REALLY CAN'T FORGET TO EDIT THIS THIS TIME FO REAL FO REAL
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "==== Welcome to My Stack / Queue Program ====" << endl << endl;
+    cout << "The function of this program is to: " << endl << endl;
+    cout << "\t1.\tValidate that a stack and a" << endl
+         << "\t\tqueue are identical." << endl
+         << "\t\tStacks / queues are the same if" << endl
+         << "\t\tthey have the same number of elements, symbols," << endl
+         << "\t\tand their elements and symbols at the" << endl
+         << "\t\tcorresponding positions are the same." << endl << endl;
+    cout << "\t2.\tThe Program also outputs whether the elements" << endl
+         << "\t\tdo have the same matching group symbols." << endl;
+
+
+    return 0;
+}
